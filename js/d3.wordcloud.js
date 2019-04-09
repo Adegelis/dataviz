6118905b5c9d3fb1b911269780dcd2b07ce61b60
fdevrainne <francois.devrainne@gmail.com>
2019-04-09 23:12:09 +0200

    Edit index.html to run zingchart

--- js/d3.wordcloud.js
+++ js/d3.wordcloud.js
@@ -48,7 +48,7 @@ var myConfig = {
 };
 
 zingchart.render({
-  id: "myChart",
+  id: "wordcloud_description_fille",
   data: myConfig,
   height: 350,
   width: 500
@@ -105,7 +105,63 @@ var myConfig = {
 };
 
 zingchart.render({
-  id: "myChart1",
+  id: "wordcloud_description_mixte",
+  data: myConfig,
+  height: 350,
+  width: 500
+});
+
+ar myConfig = {
+  type: "wordcloud",
+  options: {
+    text:
+  "Anne K Anne K Anne K Colin",
+    minLength: 1,
+    ignore: [
+      "aucun"
+    ],
+    maxItems: 40,
+    aspect: "spiral",
+    rotate: false,
+    colorType: "palette",
+    palette: [
+      "#D32F2F",
+      "#5D4037",
+      "#a83d2f",
+      "#E53935",
+      "#6D4C41",
+      "#3a2927",
+      "#F44336",
+      "#795548",
+      "#281412",
+      "#EF5350",
+      "#8D6E63",
+      "#140502"
+    ],
+
+    style: {
+      fontFamily: "Crete Round",
+
+      hoverState: {
+        borderRadius: 2
+      },
+      tooltip: {
+        text: "%text: %hits",
+        visible: true,
+
+        alpha: 0.9,
+        borderRadius: 2,
+        borderColor: "none",
+        fontFamily: "Georgia",
+        textAlpha: 1
+      }
+    }
+  },
+
+};
+
+zingchart.render({
+  id: "wordcloud_description_garcon",
   data: myConfig,
   height: 350,
   width: 500
