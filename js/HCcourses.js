323853695aed7b7188577a2b7c66b0ee7e98d4f3
Adegelis <a2gelis3@gmail.com>
2019-04-09 21:26:26 +0200

    CleaningColinJS

---
+++ js/HCcourses.js
@@ -0,0 +1,15 @@
+Highcharts.chart('hccourses',{ 
+                chart: {type: 'column'},
+                title: {text: 'Temps moyen quotidien consacré aux courses'},
+                subtitle: {text: ''},
+                xAxis: {categories: ['Sexe'],crosshair: true},yAxis: {min: 0,title: {text: 'Temps (minutes)'}},
+                tooltip: {
+                  headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                  pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' + '<td style="padding:0"><b>{point.y:f} min</b></td></tr>',
+                  footerFormat: '</table>',
+                  shared: true,
+                  useHTML: true
+                },
+                plotOptions: {column: {pointPadding: 0.2,borderWidth: 0}},
+                series: [{name: 'Hommes',data: [18],color:'#2eaaff'}, {name: 'Femmes',data: [25],color:'#f748ea'}]
+            });
